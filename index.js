d9815479cb64744521dee7427ae44768c991bb6a
davep80 <davidpaq1@gmail.com>
2023-03-16 19:00:00 -0400

    refactor form input and line break for each

--- index.js
+++ index.js
@@ -1,52 +1,64 @@
 console.log("Code your solution!")
 
-let hone = document.querySelector('h1')
-
-hone.textContent = 'My To-Dos'
+document.querySelector('h1').textContent = 'My To-Dos'
 
 const form = document.createElement("form");
 
-    // Create the textarea element
-    const textarea = document.createElement("textarea");
-    form.appendChild(textarea);
+const textarea = document.createElement("textarea");
+textarea.setAttribute('name', 'todo')
+textarea.setAttribute('type', 'text')
+form.appendChild(textarea);
 
-    const submitBtn = document.createElement("button");
-    submitBtn['type']= 'submit';
-    submitBtn.textContent = "Submit";
-    form.appendChild(submitBtn);
+const submitBtn = document.createElement("button");
+submitBtn['type'] = 'submit';
+submitBtn.textContent = "Submit";
+form.appendChild(submitBtn);
 
+const list = document.createElement("ul");
 
-    const list = document.createElement("ul");
+form.addEventListener("submit", (event) => {
+    event.preventDefault()
+    const { todo } = event.target;
+    if (todo.value) {
+        todo.value.split('\n').forEach((item) => {
+            const textareaValue = item;
+            const listItem = document.createElement("li");
+            const textNode = document.createTextNode(textareaValue);
+            listItem.appendChild(textNode);
 
-    submitBtn.addEventListener("click", (event) => {
-      event.preventDefault()
-      if (textarea.value){
-      const textareaValue = textarea.value;
-      const listItem = document.createElement("li");
-      const textNode = document.createTextNode(textareaValue);
-      listItem.appendChild(textNode);
-      list.appendChild(listItem);
-      form.reset();
-      const sthru = document.querySelectorAll("li");
+            const delBtn = document.createElement("button");
 
-      sthru.forEach((item) => {
-      item.addEventListener("click", () => {
+            delBtn.textContent = "delete";
 
-        if (!item.style.textDecoration) item.style.textDecoration = 'line-through';
-        else if (item.style.textDecoration) item.style.textDecoration = 'none'
-      });
-    });
+            listItem.appendChild(delBtn)
+            list.appendChild(listItem);
+
+            delBtn.addEventListener("click", () => {
+                listItem.remove()
+            })
+
+            form.reset();
+            const sthru = document.querySelectorAll("li");
+
+            sthru.forEach((item) => {
+                item.addEventListener("click", () => {
+                    if (!item.style.textDecoration) item.style.textDecoration = 'line-through';
+                    else if (item.style.textDecoration) item.style.textDecoration = 'none'
+                });
+            });
+        }
+        )
     } else {
 
         let errorp = document.querySelector('p')
         errorp.textContent = 'Error. Todo cannot be empty'
         form.reset()
     }
-    });
+});
 
-    document.body.appendChild(form);
-    document.body.appendChild(list);
-    document.querySelector('body').append(document.createElement('p'))
+document.body.appendChild(form);
+document.body.appendChild(list);
+document.querySelector('body').append(document.createElement('p'))
 
 
 
