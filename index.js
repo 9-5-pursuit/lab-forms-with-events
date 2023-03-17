0721dfd822d95e20de5faf482a26d23ae07f23d3
Nicole Rodriguez <nicolerodriguez@Nicoles-Air.lan>
2023-03-17 00:58:45 -0400

    i dont know about this one

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-console.log("Code your solution!")
+console.log("Code your solution!");
+// write code below
+
+//create variables to store our elements
+const form = document.querySelector("#todo-form");
+const todoInput = document.querySelector("#todo-input");
+const errorMessage = document.getElementById("error-message");
+const todoList = document.getElementById("todo-list");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const todoText = todoInput.value.trim();
+  if (todoText !== "") {
+    const newTodo = document.createElement("li");
+    newTodo.textContent = todoText;
+
+    newTodo.addEventListener("click", () => {
+      newTodo.classList.toggle("completed");
+    });
+    todoList.append(newTodo);
+    todoInput.value = "";
+    errorMessage.textContent = "";
+  } else {
+    errorMessage.textContent = "Error. Todo cannot be empty";
+  }
+});
