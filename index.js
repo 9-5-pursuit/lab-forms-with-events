5a82ba5f8e8c89757e16fc8907b40b9d0dd098f5
thandi bristol <thandibristol@thandis-air.mynetworksettings.com>
2023-03-17 09:45:16 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 console.log("Code your solution!")
+const form = document.querySelector("form")
+const list = document.querySelector("#list ul")
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    event.target.todo
+    console.log(todo)
+});
+
+const todoList1 = document.createElement("li");
+
+todoList1.textContent = 'todo.value'
+
+
+
+
+
+list.append(todoList1)
