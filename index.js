6d7ac8caefe1998d2a62c1c3bcf0f4f8eb38ab83
zazimov <zalmanazimov@pursuit.org>
2023-03-16 22:09:56 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-console.log("Code your solution!")
+// console.log("Code your solution!")
+
+const form = document.querySelector("form");
+const list = document.querySelector("body ul");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const body = document.querySelector("body");
+  if (document.querySelector("p")) {
+    document.querySelector("p").remove();
+  }
+  //   console.log(event);
+
+  const newListItem = document.createElement("li");
+  //   this will target the name of the input, NOT the id
+  newListItem.textContent = event.target["new-task"].value;
+  //   newListItem.textContent = document.getElementById("new-task").value;
+  if (event.target["new-task"].value) {
+    list.append(newListItem);
+  } else {
+    const pTag = document.createElement("p");
+    pTag.innerText = "Error: To-do cannot be empty";
+    body.append(pTag);
+  }
+
+  let listItemsArr = document.querySelectorAll("li");
+
+  for (let item of listItemsArr) {
+    item.addEventListener("click", () => {
+      item.style.textDecoration = "line-through";
+    });
+  }
+
+  event.target.reset();
+});
