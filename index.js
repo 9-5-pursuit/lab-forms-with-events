1a1549fa0ccad5e956b37ab77ce047fce41f1c3f
Fabio Tavarez <fabiotavarez@pursuit.org>
2023-03-17 03:45:48 -0400

    Lab finished

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-console.log("Code your solution!")
+console.log("Code your solution!");
+
+const form = document.querySelector("form");
+
+const list = document.querySelector("ul");
+
+const pTag = document.querySelector("p");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const listItem = document.createElement("li");
+
+  if (event.target.todo.value === "") {
+    pTag.innerText = "Error. Todo cannot be empty";
+  } else {
+    listItem.innerText = event.target.todo.value;
+
+    list.append(listItem);
+  }
+
+  const liTag = document.querySelectorAll("li");
+
+  liTag.forEach((element) => {
+    element.addEventListener("click", (item) => {
+      item.target.style.textDecoration = "line-through solid";
+    });
+  });
+
+  form.reset();
+});
