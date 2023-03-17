d263c293e5539283fbf0a7af6be12ca9c1ab83c1
Jennifer <jenniferpeterson@pursuit.org>
2023-03-16 22:08:19 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-console.log("Code your solution!")
+const form = document.querySelector("#myForm");
+// console.log(form);
+
+let body = document.querySelector("body");
+
+//we want something to happen when the submit button is pressed
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  // console.log(event.target);
+  if (document.querySelector("p")) {
+    document.querySelector("p").remove();
+  }
+  console.log(event);
+
+  const textInput = event.target.box.value; //include .value to access the value of the text
+  // console.log(textInput);
+  const listItem = document.createElement("li");
+
+  listItem.textContent = textInput;
+  const list = document.querySelector("ul");
+
+  if (textInput.length) {
+    list.append(listItem);
+  } else {
+    const pTag = document.createElement("p");
+    pTag.textContent = "You must enter text here!";
+
+    body.append(pTag);
+  }
+
+  let taskComplete = document.querySelectorAll("li");
+  for (let task of taskComplete) {
+    task.addEventListener("click", () => {
+      task.style.textDecoration = "line-through";
+    });
+  }
+
+  event.target.reset();
+});
