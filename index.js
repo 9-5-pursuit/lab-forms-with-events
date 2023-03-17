2799006fecdbd381e065dd57c84ee381194bf9c3
Jameel Ibrahim <jameelibrahim@Jameels-MacBook-Air.local>
2023-03-17 09:05:30 -0400

    Not Completed

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("Code your solution!")
+//console.log("Code your solution!")
+const form = document.querySelector("body form");
+//console.log(form);
+const list = document.querySelector("body ul");
+//console.log(list);
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+      
+    let lists = document.createElement("li");
+    lists.textContent = event.target.todo.value;
+    
+    
+});
