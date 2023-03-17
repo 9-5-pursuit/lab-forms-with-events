e31abd4d469dd66a0fab8ac2bfc039d9a5d74a42
Mussie <mussierussom@pursuit.org>
2023-03-17 10:01:16 -0400

    lab-forms-with-events

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("Code your solution!")
+//console.log("CODE YOUR SOLUTION");
+
+const body = document.querySelector("body");
+const form = document.querySelector("form");
+const uls = body.querySelector("ul");
+form.addEventListener("submit", (e) => {
+  console.log(e);
+  e.preventDefault();
+
+  const lis = document.createElement("li");
+  lis.textContent = e.target.input.value;
+  if (e.target.input.value) {
+    uls.append(lis);
+  } else {
+    pTag = document.createElement("p");
+    pTag.innerText = "Error: input can't be empty";
+    body.append(pTag);
+  }
+});
