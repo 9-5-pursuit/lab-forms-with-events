f730a2ff9160f3fc8c587f1facb6c2f139fe0425
Hillary Brito <hillarybrito@hillarys-air.myfiosgateway.com>
2023-03-17 01:03:46 -0400

    Bruh spec passed 2 bonuses passed

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-console.log("Code your solution!")
+console.log("Code your solution!");
+// script.js
+let form = document.querySelector("form");
+let taskInput = document.querySelector("textarea[type='text']");
+
+let subButton = document.querySelector("button[type='submit']");
+// const newLi = document.createElement("li");
+subButton.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  const ul = document.querySelector("ul");
+  let p = document.createElement("p");
+  if (taskInput.value === "") {
+    p.textContent = "Error: Must provide a task!";
+    ul.prepend(p);
+  } else {
+    const newLi = document.createElement("li");
+    newLi.textContent = taskInput.value;
+    ul.append(newLi);
+    const deleteButton = document.createElement("button");
+    deleteButton.textContent = "Delete";
+    newLi.append(deleteButton);
+    deleteButton.addEventListener("click", (event) => {
+      newLi.remove();
+    });
+  }
+  const lis = document.querySelectorAll("li");
+  for (let li of lis) {
+    li.addEventListener("click", (event) => {
+      if (li.style.textDecoration === "line-through") {
+        li.style.textDecoration = "none";
+      } else {
+        li.style.textDecoration = "line-through";
+      }
+    });
+  }
+  form.reset();
+});
