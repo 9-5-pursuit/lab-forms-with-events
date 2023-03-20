e372a4aefc9d4b2e95a400bf14ef0fc41f229ca4
Jibon P <jibonp@Jibons-MacBook-Air.local>
2023-03-20 11:06:35 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log("Code your solution!")
+const todoList = document.getElementById("todo-list");
+const todoForm = document.getElementById("todo-form");
+const todoInput = document.getElementById("todo-input");
+const errorMessage = document.getElementById("error-message");
+
+todoForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+  const todoText = todoInput.value;
+  if (todoText.trim() === "") {
+    errorMessage.textContent = "Error. Todo cannot be empty.";
+    return;
+  }
+  errorMessage.textContent = "";
+  const newTodo = document.createElement("li");
+  newTodo.textContent = todoText;
+  newTodo.addEventListener("click", function () {
+    newTodo.style.textDecoration = "line-through";
+  });
+  todoList.append(newTodo);
+  todoInput.value = "";
+
+  form.reset();
+});
