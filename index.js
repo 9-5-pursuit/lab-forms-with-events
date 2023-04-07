4f69ba3b9d593aebfe668aea9e0f15152ee81e03
Blui322 <luistejada@pursuit.com>
2023-04-07 17:58:23 -0400

    fixed

--- index.js
+++ index.js
@@ -1,45 +1,62 @@
-console.log("Code your solution!")
+console.log("Code your solution!");
+
+const h1 = document.querySelector("h1");
+h1.textContent = "Todos";
+
+const hr = document.createElement("hr");
+
+const form = document.createElement("form");
 
 const body = document.querySelector("body");
 
-
-const h1 = document.querySelector("h1");
-h1.textContent = "My To-Dos"
-
-
-const hr = document.createElement("hr");
 body.append(hr);
 
-
-const form = document.createElement("form");
-body.append(form);
-
-
 const div = document.createElement("div");
-div.classList.add("form-field");
-form.appendChild(div);
 
+div.classList.add("form-field");
+
+div.appendChild(form);
+
+body.append(div);
 
 const label = document.createElement("label");
-label.setAttribute("for", "my to-dos");
-div.append(label);
+
+label.setAttribute("for", "todos");
+
+form.append(label);
 
 const input = document.createElement("input");
-input.setAttribute("id", "my to-dos");
-input.setAttribute("name", "my to-dos");
+
+input.setAttribute("id", "todo");
+input.setAttribute("name", "todos");
 input.setAttribute("type", "text");
-div.append(input);
+form.append(input);
 
-const inputing = document.createElement("input");
-inputing.setAttribute("type", "submit");
-inputing.setAttribute("value", "submit");
-div.append(inputing);
+const input2 = document.createElement("input");
+input2.setAttribute("type", "submit");
+input2.setAttribute("value", "submit");
+input2.setAttribute("name", "submit");
 
-const diving =
-document.createElement(div);
-body.append(diving);
-diving.setAttribute("class", "form-field");
+form.append(input2);
+
+const div2 = document.createElement("div");
+body.append(div2);
+div2.setAttribute("class", "list-of-todos");
+
 
 const ul = document.createElement("ul");
-diving.append(ul);
 
+div2.append(ul);
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const list = event.target.todo.value;
+ console.log(list);
+
+   const listOfTodos = document.createElement("li");
+   listOfTodos.textContent = list
+   ul.appendChild(listOfTodos);
+
+  
+});
